adf556dc563ef8b8c7642359b4ea7828e2a42575
fifty1-fifty <carterharman9@gmail.com>
2025-03-14 15:23:11 -0400

    Update and rename Home to Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import PageTitle from '../components/PageTitle.tsx';
+
+const HomePage = () =>
+{
+    return(
+      <div>
+         <PageTitle />
+     </div>
+    );
+};
+export default HomePage;
