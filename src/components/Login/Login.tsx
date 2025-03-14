3fb592c3021805f4b0b9b536001336d888741ec2
fifty1-fifty <carterharman9@gmail.com>
2025-03-14 15:03:50 -0400

    Update Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -37,20 +37,7 @@ function Login()
     async function doLogin(event:any) : Promise<void>
     {
 	    console.log("im gat");
-		var loginState = validLogin(login, password, email);
-		if(loginState == 0)
-		{
-			console.log("iamahga");
-			setMessage("Invalid Credentials");
-			return;
-		}
-		else if(loginState == 1)
-		{
-			setMessage("Login credentials are blank");
-		}
-			
-		
-		
+	
         	event.preventDefault();
         	var obj = {login:loginName,password:loginPassword,email:emailName};
         	var js = JSON.stringify(obj);
@@ -84,39 +71,7 @@ function Login()
         	}
     };
 
-	function validLogin(username, password, email)
-	{
-		if(username == "")
-		{
-			return 0;
-			//res.status(401).json({ error: "First name is blank" });
-		}
-		
-		if(password == "")
-		{
-			return 0;
-			//res.status(401).json({ error: "Last name is blank" });
-		}
-		
-		if(email == "")
-		{
-			return 0;
-			//res.status(401).json({ error: "Email name is blank" });
-		}
-		else
-		{
-			var regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-			if(regex.test(regemail) == false)
-			{
-				return 0;
-				//res.status(401).json({ error: "Email is invalid" });
-			}
-			else
-			{
-				return 1;
-			}
-		}
-	}
+
 	
 
   /*  async function newLogin(newLoginData:JSON) : Promise<void>
