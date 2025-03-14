3fb5404edf70e0b9e172ab73afed122a5d9e420d
fifty1-fifty <carterharman9@gmail.com>
2025-03-14 15:49:14 -0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
 import PageTitle from '../components/PageTitle.tsx';
+import Home from '../components/Home/Home.tsx';
+
 
 const HomePage = () =>
 {
     return(
       <div>
          <PageTitle />
+          <Home />
      </div>
     );
 };
