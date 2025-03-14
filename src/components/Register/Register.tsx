db786f9d5833e0d938913bb2c1eb969daab94617
fifty1-fifty <carterharman9@gmail.com>
2025-03-14 16:11:16 -0400

    Update Register.tsx

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,13 +1,1 @@
-import PageTitle from '../components/PageTitle.tsx';
-import Register from '../components/Register/Register.tsx';
 
-const RegisterPage = () =>
-{
-    return(
-      <div>
-         <PageTitle />
-         <Register />
-     </div>
-    );
-};
-export default RegisterPage;
