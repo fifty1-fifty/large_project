5125787235bad171cdc2e6ab507b415f09b49a03
fifty1-fifty <carterharman9@gmail.com>
2025-03-17 15:48:16 -0400

    Update Mediator.tsx

--- src/components/Explore/Mediator.tsx
+++ src/components/Explore/Mediator.tsx
@@ -1,4 +1,4 @@
-//import React from 'react';
+/*//import React from 'react';
 import Search_Ex from './Search_Ex';
 
 const Mediator = () => {
@@ -12,4 +12,4 @@ const Mediator = () => {
     );
 };
 
-export default Mediator;
+export default Mediator; */
