f89988a3a84eea315eb9249aab2a7060a1683cc5
fifty1-fifty <carterharman9@gmail.com>
2025-03-17 15:24:17 -0400

    Update Mediator.tsx

--- src/components/Explore/Mediator.tsx
+++ src/components/Explore/Mediator.tsx
@@ -3,6 +3,7 @@ import Search_Ex from './Search_Ex';
 
 const Mediator = () => {
   return
+  (
     <div className="Mediator">
             <h1>testtesttest</h1>
             <h3>This is App.js Component</h3>
