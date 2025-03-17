3e8cb1f7776096df202b4c564fa417232a7acf08
fifty1-fifty <carterharman9@gmail.com>
2025-03-17 15:14:23 -0400

    Update Mediator.tsx

--- src/components/Explore/Mediator.tsx
+++ src/components/Explore/Mediator.tsx
@@ -1,31 +1,14 @@
-import { React, useState, createContext } from "react";
-//import "./index.css";
-import Search_Ex from "./Search_Ex";
-//import "./App.css";
-import Gallery_Ex from "./Gallery_Ex";
+import React from 'react';
+import Search_Ex from './Search_Ex';
 
-export const newContext = createContext();
-
-const newContextProvider = ({ galley }) => {
-  const [name, setName] = useState(undefined);
-
- return (
-        <newContext.Provider value={{ name, setName }}>
-            {gallery}
-        </newContext.Provider>
-    );
-};
-
-const App = () => {
-    return (
-        <div className="Mediator">
-            <h1>i want to die</h1>
-            <newContextProvider>
-                <Search_Ex />
-                <Gallery_Ex />
-            </newContextProvider>
+const Mediator = () => {
+  return
+    <div className="Mediator">
+            <h1>testtesttest</h1>
+            <h3>This is App.js Component</h3>
+            <Parent />
         </div>
     );
 };
 
-export default Mediator;
+export
