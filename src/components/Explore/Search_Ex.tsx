c4116b3d349930d4607c3d2665638ac12fce8eef
fifty1-fifty <carterharman9@gmail.com>
2025-03-17 11:49:10 -0400

    Update Search_Ex.tsx

--- src/components/Explore/Search_Ex.tsx
+++ src/components/Explore/Search_Ex.tsx
@@ -1,10 +1,9 @@
-//import React, { useState } from 'react';
-//import isEmail from 'isemail';
+import React, { useState } from 'react';
+
+import "./Search_Ex.css"
 
 
-/*const app_name = 'group22cop4331c.xyz';
-
-
+const app_name = 'group22cop4331c.xyz';
 function buildPath(route:string) : string
 {
     if (process.env.NODE_ENV != 'development')
@@ -15,18 +14,26 @@ function buildPath(route:string) : string
     {
         return 'http://localhost:5000/' + route;
     }
-} */
-import "./Search_Ex.css"
+} 
 
-function Search()
+
+const Search_Ex = ({ fetchDatra }) =>
 {
+    const [search, setSearch] = useState(''):
+    const handleSearch = () => 
+    {
+        fetchData(search);
+    };
+            
+
 
     return (
         <div className="search-container">
             <label htmlFor="searchbar">Discover</label>
             <div className="form-group">
-                <input type="text" id="searchbar" placeholder="Discover"/>
+                <input type="text" id="searchbar" placeholder="Discover" value = {input} onChange{(e) => setInput(e.target.value)}/>
             </div>
+                <button onClick={handleSearch}>Search</button>
         </div>
 
     );
