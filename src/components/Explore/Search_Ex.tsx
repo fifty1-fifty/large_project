504aa8b944c8aa3d026aba358cfa3608fbf45d5b
fifty1-fifty <carterharman9@gmail.com>
2025-03-17 12:40:51 -0400

    Update Search_Ex.tsx

--- src/components/Explore/Search_Ex.tsx
+++ src/components/Explore/Search_Ex.tsx
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
+//import isEmail from 'isemail';
 
-const SearchComponent = ({ fetchData }) => {
-  const [input, setInput] = useState('');
 
-  const handleSearch = () => {
-    fetchData(input); // Calls API request function in parent
-  };
+/*const app_name = 'group22cop4331c.xyz';
 
-  return (
-    <div>
-      <input 
-        type="text" 
-        placeholder="Search for a movie..." 
-        value={input} 
-        onChange={(e) => setInput(e.target.value)} 
-      />
-      <button onClick={handleSearch}>Search</button>
-    </div>
-  );
-};
 
-export default SearchComponent;
+function buildPath(route:string) : string
+{
+    if (process.env.NODE_ENV != 'development')
+    {
+        return 'http://' + app_name + route;
+    }
+    else
+    {
+        return 'http://localhost:5000/' + route;
+    }
+} */
+import "./Search_Ex.css"
+
+function Search()
+{
+
+    return (
+        <div className="search-container">
+            <label htmlFor="searchbar">Discover</label>
+            <div className="form-group">
+                <input type="text" id="searchbar" placeholder="Discover"/>
+            </div>
+        </div>
+
+    );
+}
+export default Search;
