0e168f663dba0bfd112cc2b8408c438f72a39f8d
fifty1-fifty <carterharman9@gmail.com>
2025-03-18 11:41:32 -0400

    Update ExplorePage.tsx

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,7 +1,7 @@
 //import Gallery_Ex from '../components/Explore/Gallery_Ex.tsx';
 //import Search_Ex from '../components/Explore/Search_Ex.tsx';
 //import Navigation from '../components/Navigation/Navigation.tsx';
-import Mediator from '../components/Explore/Mediator.tsx;
+import Mediator from '../components/Explore/Mediator.tsx';
 
 
 const ExplorePage = () =>
