8fa17b91b7010f1e96f0d61d1925cc978b3968e2
Jared Luzod <luzodj123789@gmail.com>
2025-03-19 22:12:01 -0400

    css stuff

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -140,7 +140,7 @@ function Register()
 
         
             <div className="form-group">
-                <label htmlFor="registerUserName">Login</label>
+                <label htmlFor="registerUserName">Username</label>
                 <input type="text" id="registerUserName" placeholder="New Username" value={registerUserName}
                     onChange={handleSetRegisterUserName} />
                 {usernameError && <span className="error-message">{usernameError}</span>}
