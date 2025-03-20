79556876b287f068d713c761b38835fc0812e714
fifty1-fifty <carterharman9@gmail.com>
2025-03-20 14:35:34 -0400

    Update Gallery_Ex.tsx

--- src/components/Explore/Gallery_Ex.tsx
+++ src/components/Explore/Gallery_Ex.tsx
@@ -1,19 +1,39 @@
 //import React from "react";
 //<h5>{JSON.stringify(props.data)}</h5>
-const Gallery_Ex = ( {posters} : {posters : string[]}) => {
-    console.log(posters.length);
-    return (
+const Gallery_Ex = ( {movies} : {movies : any) => {
+
+
+    let posters: string[] = [];
+    for(let i = 0 ; i < movies.length ; i++)
+    {
+       // posters[i] = ;
+       posters.push(movies[i].poster_path)
+    } 
+
+    function gotoInfoPage(selectedMovie : number)
+    {
+        //window.location.href = '/explore';
+        console.log(movies[selectedMovie].id);
+
+        window.location.href = `/movie?movieId=${movies[selectedMovie].id}`;
+    }
+
+
+
+    
+       return ( 
         <div className="container">
-            <div className="row">
-                {posters.map((poster : string, index : number) => (
-                    <div key={index} className="col-sm-2 mb-4">
-                        <div className="photo-card">
-                            <img src={`https://image.tmdb.org/t/p/original${poster}`} className="card-img-top img-fluid" alt={`Movie Poster ${index}`} />
-                        </div>
-                    </div>
-                ))}
+        <div className="row">
+            {posters.map((poster : string, index : number) => (
+                <div key={index} className="col-sm-2 mb-4">
+                    <div className="photo-card">
+                <img src={`https://image.tmdb.org/t/p/original${poster}`} id="poster" className="card-img-top img-fluid" alt={"No Movies"} onClick={() => gotoInfoPage(index)} style={{ cursor: "pointer"}}/>
             </div>
         </div>
+    ))}
+</div>
+</div> 
+    
     );
 };
 
