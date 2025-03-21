a7eff62874449fe3468cbb5331e810b6e84de654
milezdav <85088686+milezdav@users.noreply.github.com>
2025-03-21 00:51:06 -0400

    style update

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -5,7 +5,7 @@ const ExplorePage = () => {
     return(
         <div>
             <PageTitle />
-            <h2>In progress</h2>
+            <h2 style={{ color: '#0e6dfb'}}>In progress</h2>
         </div>
     );
 };
