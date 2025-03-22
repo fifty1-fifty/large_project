4b9a41d8899a1ca6be64d8f5009fee87d7ab58c5
Calvin <calvincruz1223@gmail.com>
2025-03-21 21:25:50 -0400

    example app kinda

--- movieapp/lib/main.dart
+++ movieapp/lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:movieapp/routes/routes.dart';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-// This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: '',
-debugShowCheckedModeBanner: false,
-theme: ThemeData(),
-routes: Routes.getroutes,
-);
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          titleTextStyle: TextStyle(
+            package: "Movie App",
+          color: Colors.grey
+            
+          ),
+          backgroundColor: Colors.black,
+          title: Center(child: const Text("Movie App")),
+        ),
+      ),
+    );
+  }
 }
-}
+
+
+// class MyApp extends StatelessWidget {
+// // This widget is the root of your application.
+// @override
+// Widget build(BuildContext context) {
+// return MaterialApp(
+// title: '',
+// debugShowCheckedModeBanner: false,
+// theme: ThemeData(),
+// routes: Routes.getroutes,
+// );
+// }
+// }
