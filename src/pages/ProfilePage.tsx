b83b8dc7c74ef5b7aa8160fbf9b8a5e5812737a1
milezdav <85088686+milezdav@users.noreply.github.com>
2025-03-24 19:15:02 -0400

    Update ProfilePage.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ProfileInfo from "../components/Profile/ProfileInfo"; 
-import "./index.css"; 
+import "../index.css"; 
 
 const ProfilePage: React.FC = () => {
   const navigate = useNavigate();
