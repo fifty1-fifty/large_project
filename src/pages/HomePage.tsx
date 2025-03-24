8e04de8f1bf0df65b41b52da590ec95286b21008
fifty1-fifty <carterharman9@gmail.com>
2025-03-24 14:52:35 -0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 //import Gallery_Ex from '../components/Explore/Gallery_Ex.tsx';
 //import Search_Ex from '../components/Explore/Search_Ex.tsx';
 import Mediator from '../components/Home/Mediator.tsx';
+import Background from '../components/Login/Background.tsx';
 
 
 const HomePage = () =>
@@ -8,6 +9,7 @@ const HomePage = () =>
     return(
         <div>
             <Mediator />
+            <Background />
         </div>
         );
 }
