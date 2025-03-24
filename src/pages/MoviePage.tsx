7a1b14bd97378e85e5bc86b762deaed0c73b482c
fifty1-fifty <carterharman9@gmail.com>
2025-03-24 15:21:24 -0400

    Update MoviePage.tsx

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,9 +1,11 @@
-import Info from '../components/MovieInfo/MovieInfo'
+import Info from '../components/MovieInfo/MovieInfo';
+import Navigation from '../components/Navigation/Navigation.tsx';
 
 const InfoPage = () =>
 {
     return(
       <div>
+        <Navigation />
         <Info />
      </div>
     );
