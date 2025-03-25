9ace4d8e8928c0740364a366fcf87149499c0bc7
Calvin <calvincruz1223@gmail.com>
2025-03-25 02:31:22 -0400

    changed cardscreen and loginscreen with updated function for raisedbutton

--- movieapp/lib/main.dart
+++ movieapp/lib/main.dart
@@ -5,37 +5,39 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          titleTextStyle: TextStyle(
-            package: "Movie App",
-          color: Colors.grey
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           titleTextStyle: TextStyle(
+//             package: "Movie App",
+//           color: Colors.grey
             
-          ),
-          backgroundColor: Colors.black,
-          title: Center(child: const Text("Movie App")),
-        ),
-      ),
-    );
-  }
+//           ),
+//           backgroundColor: Colors.black,
+//           title: Center(child: const Text("Movie App")),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+title: '',
+debugShowCheckedModeBanner: false,
+theme: ThemeData(),
+routes: Routes.getroutes,
+);
+}
 }
 
 
-// class MyApp extends StatelessWidget {
-// // This widget is the root of your application.
-// @override
-// Widget build(BuildContext context) {
-// return MaterialApp(
-// title: '',
-// debugShowCheckedModeBanner: false,
-// theme: ThemeData(),
-// routes: Routes.getroutes,
-// );
-// }
-// }
--- movieapp/lib/screens/LoginScreen.dart
+++ movieapp/lib/screens/LoginScreen.dart
@@ -1,32 +1,49 @@
 import 'package:flutter/material.dart';
+
 class LoginScreen extends StatefulWidget {
-@override
-_LoginScreenState createState() => _LoginScreenState();
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
 }
+
 class _LoginScreenState extends State<LoginScreen> {
-@override
-void initState() {
-super.initState();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.blue, body: MainPage());
+  }
 }
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-backgroundColor: Colors.blue,
-body: MainPage(),
-);
-}
-}
+
 class MainPage extends StatefulWidget {
-@override
-_MainPageState createState() => _MainPageState();
+  @override
+  _MainPageState createState() => _MainPageState();
 }
+
 class _MainPageState extends State<MainPage> {
-@override
-void initState() {
-super.initState();
-}
-@override
-Widget build(BuildContext context) {
-return Container();
-}
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/cards');
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor:
+              Colors.brown[50], // Background color (formerly "primary")
+          foregroundColor: Colors.black, // Text color (formerly "onPrimary")
+          padding: EdgeInsets.all(2.0),
+          splashFactory: InkRipple.splashFactory, // Ripple effect (optional)
+        ),
+        child: Text('Do Login'),
+      ),
+    );
+  }
 }
