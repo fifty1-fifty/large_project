9973d4cdf21093c3e84796082939f13312513b15
fifty1-fifty <carterharman9@gmail.com>
2025-04-02 18:35:29 -0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,4 @@
-//import Gallery_Ex from '../components/Explore/Gallery_Ex.tsx';
-//import Search_Ex from '../components/Explore/Search_Ex.tsx';
 import Mediator from '../components/Home/Mediator.tsx';
-import Background from '../components/Login/Background.tsx';
-
-
 const HomePage = () =>
 {
     return(
