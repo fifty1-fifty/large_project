c62992aeb421a7888c039e23744bbb9ff9d45336
milezdav <85088686+milezdav@users.noreply.github.com>
2025-04-02 18:41:26 -0400

    Revert "Merge branch 'main' of https://github.com/fifty1-fifty/large_project"
    This reverts commit 86e5cf1e6ff6b718cf47e075c311ca158548d2c8, reversing
    changes made to 71a62121e6b4288f003ff2fe5aa10e002cb8e896.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,29 +1,40 @@
 import React from "react";
-import { Link }  from "react-router-dom";
+import { Link, useNavigate }  from "react-router-dom";
 import './Navigation.css'
 
 const Navbar: React.FC = () => {
 
+  // Logout
+   const navigate = useNavigate();
+   const handleLogout = () => {
 
+      // remove user from local storage
+      localStorage.removeItem("user_data");
+      navigate("/login");
+   }
 
   return (
     <nav className="navbar navbar-expand-lg custom-navbar fixed-top">
       <div className="container-fluid">
 
+        <Link className="navbar-brand" id="syncopate-regular" to="/">FLICKS</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto" id="buttonContainer">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link active" id="linkButton" to="/"><i id="con" className="material-icons">holiday_village</i>Home</Link>
+              <Link className="nav-link active" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" id="linkButton" to="/explore"><i id="con" className="material-icons">explore</i>Explore</Link>
+              <Link className="nav-link" to="/explore">Explore</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" id="linkButton" to="/profile"><i id="con" className="material-icons">people_alt</i>Profile</Link>
+              <Link className="nav-link" to="/profile">Profile</Link>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-primary ms-2" onClick={handleLogout}>Log Out</button>
             </li>
           </ul>
         </div>
