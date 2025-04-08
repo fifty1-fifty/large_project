c1a34bd2061b7e6d29971106fac13e1b5ac2af47
fifty1-fifty <carterharman9@gmail.com>
2025-04-08 17:30:28 -0400

    Update Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -73,7 +73,7 @@ function Login()
             else
             {
                 var user =
-                {firstName:res.firstName,lastName:res.lastName,id:res.id,token}
+                {firstName:res.firstName,lastName:res.lastName,id:res.id,token:res.token}
                 localStorage.setItem('user_data', JSON.stringify(user));
                 window.location.href = '/home';
             }
