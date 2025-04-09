646ebb9c6f2ec6d0674e4ceb1f26f404b508925d
fifty1-fifty <carterharman9@gmail.com>
2025-04-09 15:53:50 -0400

    Add files via upload

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,15 +6,21 @@ import { buildPath } from '../../utils';
 
 function Login()
 {
+    const storedUser = localStorage.getItem("user_data");
+    const user = storedUser ? JSON.parse(storedUser) : {};
+    const token = user?.token;
+    if(token)
+    {
+        window.location.href = '/explore';
+    }
+
     console.log("im gat");
 
     const [emailName,setLoginEmail] = React.useState('');
-    const [loginName,setLoginName] = React.useState('');
     const [loginPassword,setPassword] = React.useState('');
 
     //login credentials validator
     const [emailError, setEmailError] = useState(' ');
-    const [usernameError, setUsernameError] = useState(' ');
     const [passwordError, setPasswordError] = useState(' ');
 
 
@@ -29,12 +35,6 @@ function Login()
             setEmailError('');
         }
 
-        if (loginName.trim() === '') {
-            setUsernameError('Username cannot be empty.');
-            valid = false;
-        } else {
-            setUsernameError('');
-        }
 
         if (loginPassword.length < 6) {
             setPasswordError('Password must be at least 6 characters.');
@@ -55,7 +55,7 @@ function Login()
             return;
         }
 
-        var obj = {login:loginName,password:loginPassword,email:emailName};
+        var obj = {password:loginPassword,email:emailName};
         var js = JSON.stringify(obj);
         try
         {
@@ -63,9 +63,8 @@ function Login()
             {method:'POST',body:js,headers:{'Content-Type': 'application/json'}})
             var res = JSON.parse(await response.text());
 
-
             console.log(res.id);
-	    console.log(res.Token);
+	        console.log(res.token);
 		
             if( res.id <= 0 )
             {
@@ -74,9 +73,9 @@ function Login()
             else
             {
                 var user =
-                {firstName:res.firstName,lastName:res.lastName,id:res.id,Token:res.token}
+                {firstName:res.firstName,lastName:res.lastName,id:res.id,token:res.token}
                 localStorage.setItem('user_data', JSON.stringify(user));
-                window.location.href = '/home';
+                window.location.href = '/explore';
             }
         }
         catch(error:any)
@@ -116,13 +115,6 @@ function Login()
             return;
         }
     };*/
-
-
-    function handleSetLoginName( e: any ) : void
-    {   
-        setLoginName( e.target.value );
-    }
-
     function handleSetPassword( e: any ) : void
     {
         setPassword( e.target.value );
@@ -152,14 +144,9 @@ function Login()
             <div className="login-container">
 
                 <div className="form-group" id="getridoftheannoyingbackground">
-                     <input type="text" id="input" placeholder="Email or Username"
+                     <input type="text" id="input" placeholder="Email"
                       onChange={handleSetLoginEmail} />
                  </div>
-
-                <div className="form-group" id="getridoftheannoyingbackground">
-                    <input type="text" id="input" placeholder="Username"
-                    onChange={handleSetLoginName} />
-                </div>
 
                 <div className="form-group" id="getridoftheannoyingbackground">
                     <input type="password" id="input" placeholder="Password"
@@ -170,7 +157,6 @@ function Login()
 
                 <div className="form-group" id="error-dv">
                      {emailError && <span className="error-mess" id="error" >{emailError}</span>}
-		              {usernameError && <span className="error-mess" id="error" >{usernameError}</span>}
 		              {passwordError && <span className="error-mess" id="error" >{passwordError}</span>}
                 </div>
 
