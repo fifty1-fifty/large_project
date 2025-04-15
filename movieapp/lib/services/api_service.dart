4809cedc4801aa15fa29572d9452d78a44e39b40
Calvin <calvincruz1223@gmail.com>
2025-04-15 19:46:53 -0400

    implemented updated login and register API's

--- movieapp/lib/services/api_service.dart
+++ movieapp/lib/services/api_service.dart
@@ -7,62 +7,61 @@ class ApiService {
   static const String _baseUrl = 'http://group22cop4331c.xyz:5000/api';
 
   //Login
-  static Future<Map<String, dynamic>> login(
-    String email,
-    String password,
-    String login,
-  ) async {
-    try {
-      final response = await http.post(
-        Uri.parse('$_baseUrl/login'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'email': email, 'password': password, 'login': " "}),
-      );
+  static Future<Map<String, dynamic>> login({
+    required String email,
+    required String password,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
 
-      final responseData = jsonDecode(response.body);
-      if (response.statusCode == 200) {
-        if (responseData['error']?.isNotEmpty ?? false) {
-          throw Exception(responseData['error']);
-        }
-        return responseData;
-      } else {
-        throw Exception(responseData['message'] ?? 'Login failed');
-      }
-    } catch (e) {
-      throw Exception('Login error: $e');
+    final data = _handleResponse(response);
+
+    // Extra check in case backend returns an invalid id (e.g. -1)
+    if (data['id'] == -1) {
+      throw Exception('Invalid email or password');
     }
+
+    return {
+      'id': data['id'],
+      'firstName': data['firstName'],
+      'lastName': data['lastName'],
+      'token': data['token'],
+      'verified': data['verified'],
+    };
   }
 
   //Register
   static Future<Map<String, dynamic>> register({
-  required String email,
-  required String password,
-  required String firstName,
-  required String lastName,
-  required String username,
-}) async {
-  final response = await http.post(
-    Uri.parse('$_baseUrl/register'),
-    headers: {'Content-Type': 'application/json'},
-    body: jsonEncode({
-      'regemail': email,
-      'regpassword': password,
-      'first': firstName,
-      'last': lastName,
-      'reglogin': username,
-    }),
-  );
+    required String email,
+    required String password,
+    required String firstName,
+    required String lastName,
+    required String username,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'regemail': email,
+        'regpassword': password,
+        'first': firstName,
+        'last': lastName,
+        'reglogin': username,
+      }),
+    );
 
-  final data = _handleResponse(response);
+    final data = _handleResponse(response);
 
-  // The response should include: { error: "", login: ..., password: ... }
-  if (data is Map<String, dynamic>) {
-    return data;
-  } else {
-    throw Exception('Unexpected response format during registration.');
+    // The response should include: { error: "", login: ..., password: ... }
+    if (data is Map<String, dynamic>) {
+      return data;
+    } else {
+      throw Exception('Unexpected response format during registration.');
+    }
   }
-}
-
 
   //Get trending movies
   static Future<List<Movie>> trendingMovie({
