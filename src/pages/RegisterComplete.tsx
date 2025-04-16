ecdc86ca39a9df315f91e6f0861c9bdf4c810549
fifty1-fifty <carterharman9@gmail.com>
2025-04-16 11:17:06 -0400

    Update RegisterComplete.tsx

--- src/pages/RegisterComplete.tsx
+++ src/pages/RegisterComplete.tsx
@@ -1,5 +1,10 @@
 
 const RegisterComplete = () => {
+
+    for(let i = 0 ;  i < 1000 ; i++)
+        {}
+    window.location.href = '/login';
+    
     return(
         <div>
             <h2>Registration Complete. Please click the link sent to the email address provided to get started.</h2>
